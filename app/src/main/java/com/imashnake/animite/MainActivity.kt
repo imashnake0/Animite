150328b5769b7b6652493e5942bcd0dad5f85b5d
imashnake0 <kamaleshpaluru07@gmail.com>
2022-02-19 18:37:38 -0500

    Created Apollo client
    Spoiler alert: The response variable now stores the actual response. IT WORKS!

--- app/src/main/java/com/imashnake/animite/MainActivity.kt
+++ app/src/main/java/com/imashnake/animite/MainActivity.kt
@@ -3,18 +3,21 @@ package com.imashnake.animite
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.apollographql.apollo3.ApolloClient
+import com.apollographql.apollo3.api.Optional
 import com.imashnake.animite.ui.theme.AnimiteTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val apolloClient = ApolloClient.Builder()
+            .serverUrl("https://graphql.anilist.co/")
+            .build()
+
+        val response = apolloClient.query(ExampleQuery(id = Optional.presentIfNotNull(30)))
+
         setContent {
             AnimiteTheme {
                 Text("Hello")
