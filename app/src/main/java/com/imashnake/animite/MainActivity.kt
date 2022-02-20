af92f8d4ed215649953ce6846ddc987053dffa86
imashnake0 <kamaleshpaluru07@gmail.com>
2022-02-19 19:43:19 -0500

    Example logger
    Added a simple coroutine to log data for "カウボーイビバップ".

--- app/src/main/java/com/imashnake/animite/MainActivity.kt
+++ app/src/main/java/com/imashnake/animite/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.imashnake.animite
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.Text
 import com.apollographql.apollo3.ApolloClient
 import com.apollographql.apollo3.api.Optional
 import com.imashnake.animite.ui.theme.AnimiteTheme
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+private const val TAG = "MainActivity"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +22,13 @@ class MainActivity : ComponentActivity() {
             .serverUrl("https://graphql.anilist.co/")
             .build()
 
-        val response = apolloClient.query(ExampleQuery(id = Optional.presentIfNotNull(30)))
+        // TODO:
+        //  See how printing a list of these entries would work; handle errors.
+        //  Make better use of [coroutines](https://kotlinlang.org/docs/coroutines-guide.html)
+        GlobalScope.launch(Dispatchers.IO) {
+            val response = apolloClient.query(ExampleQuery(id = Optional.presentIfNotNull(1))).execute()
+            Log.d(TAG, response.data.toString())
+        }
 
         setContent {
             AnimiteTheme {
