aa9b61870d92d8efff9c604c27388c5fc76af2eb
imashnake0 <kamaleshpaluru07@gmail.com>
2022-03-09 00:08:53 -0500

    Displayed queried list of animes
    Made use of some basic coroutine features to display the list of animes. This process will improve with better architecture, since we can then make proper use of coroutines and compose.

--- app/src/main/java/com/imashnake/animite/MainActivity.kt
+++ app/src/main/java/com/imashnake/animite/MainActivity.kt
@@ -4,43 +4,56 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.lifecycleScope
 import com.apollographql.apollo3.ApolloClient
 import com.apollographql.apollo3.api.Optional
-import com.imashnake.animite.ui.theme.AnimiteTheme
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 private const val TAG = "MainActivity"
 
+// TODO: Separate logic and UI using proper app architecture.
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setContent {
-            AnimiteTheme {
-                Text("Hello")
-            }
-        }
 
         val apolloClient = ApolloClient.Builder()
             .serverUrl("https://graphql.anilist.co/")
             .build()
 
-        // TODO:
-        //  See how printing a list of these entries would work; handle errors.
-        //  Make better use of [coroutines](https://kotlinlang.org/docs/coroutines-guide.html)
-        GlobalScope.launch(Dispatchers.IO) {
-            val response = apolloClient.query(ExampleQuery(id = Optional.presentIfNotNull(1))).execute()
-            Log.d(TAG, response.data.toString())
-            Log.d(TAG, response.data?.media?.id.toString())
-            Log.d(TAG, response.data?.media?.title?.english.toString())
-            Log.d(TAG, response.data?.media?.title?.native.toString())
-            Log.d(TAG, response.data?.media?.title?.romaji.toString())
+        lifecycleScope.launch(Dispatchers.IO) {
+            val response =
+                apolloClient.query(ExampleListQuery(
+                    search = Optional.presentIfNotNull("Attack On Titan"),
+                    perPage = Optional.presentIfNotNull(20)
+                )).execute()
+
+            fun animeAtIndex(index: Int): String {
+                return response.data?.page?.media?.get(index)?.title?.native.toString()
+            }
+
+            var animeList = ""
+            for (i in 0..19) {
+                animeList += animeAtIndex(i) + "\n"
+                Log.d(TAG, animeAtIndex(i))
+            }
+
+            withContext(Dispatchers.Main) {
+                setContent {
+                    AnimeList(animeList = animeList)
+                }
+            }
         }
-
-        // Coroutines playground
-
     }
+}
+
+@Composable
+fun AnimeList(animeList: String) {
+    Text(text = animeList, modifier = Modifier.padding(30.dp))
 }
