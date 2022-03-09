67f3ffe559b2e57c289ae3dd9793c40798fad282
imashnake0 <kamaleshpaluru07@gmail.com>
2022-03-09 01:23:55 -0500

    Suppressed spelling checks
    Suppressed spelling checks to make build files easy to read.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+@file:Suppress("SpellCheckingInspection")
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
+@file:Suppress("SpellCheckingInspection")
+
 pluginManagement {
     repositories {
         gradlePluginPortal()
