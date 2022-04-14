6381a6c6c1dabe3bc27f820db9feb7f97b31753b
imashnake0 <kamaleshpaluru07@gmail.com>
2022-04-14 19:53:22 -0400

    Manually injected dependencies
    Injected dependencies manually, this still needs a lot of work since there are some unnecessary checks.

--- app/src/main/java/com/imashnake/animite/ui/elements/Home.kt
+++ app/src/main/java/com/imashnake/animite/ui/elements/Home.kt
@@ -1,12 +1,20 @@
 package com.imashnake.animite.ui.elements
 
-import android.util.Log
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
+import com.imashnake.animite.AnimeQuery
+import com.imashnake.animite.AnimiteApplication
 import com.imashnake.animite.ui.state.HomeViewModel
-
-private const val TAG = "HomeComposable"
 
 /**
  * TODO:
@@ -16,18 +24,63 @@ private const val TAG = "HomeComposable"
  */
 @Composable
 fun Home(
-    viewModel: HomeViewModel = viewModel()
+    viewModel: HomeViewModel = viewModel(factory = AnimiteApplication().container.homeViewModelFactory)
 ) {
-    Text(text = "Home")
+    viewModel.run {
+        addAnime(132405)
+    }
 
-    viewModel.fetchAnime(132405)
     val animeList = viewModel.uiState.animeList
 
-    Log.d(TAG, animeList[0]?.id.toString())
-    Log.d(TAG, animeList[0]?.type.toString())
-    Log.d(TAG, animeList[0]?.title?.english.toString())
-    Log.d(TAG, animeList[0]?.title?.native.toString())
-    Log.d(TAG, animeList[0]?.title?.romaji.toString())
-    Log.d(TAG, animeList[0]?.coverImage?.large.toString())
-    Log.d(TAG, animeList[0]?.coverImage?.extraLarge.toString())
+    if (animeList.isNotEmpty()) {
+        AnimeList(animeList = animeList)
+    } else {
+        Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
+            CircularProgressIndicator()
+        }
+    }
+}
+
+@Composable
+fun AnimeList(animeList: List<AnimeQuery.Media?>) {
+    Column(
+        modifier = Modifier
+            .verticalScroll(rememberScrollState())
+            // TODO: This is a hack, understand how layouts work and un-hardcode this.
+            .padding(bottom = 80.dp)
+            .fillMaxSize()
+    ) {
+        for (i in animeList.indices) {
+            Row {
+                AsyncImage(
+                    model = animeList[i]?.coverImage?.large,
+                    contentDescription = animeList[i]?.title?.native,
+                    modifier = Modifier
+                )
+                Column(modifier = Modifier.align(Alignment.CenterVertically)) {
+                    Text(
+                        text = (animeList[i]?.title?.english ?: "Null bro"),
+                        modifier = Modifier
+                            .padding(6.dp)
+                            .absolutePadding(left = 30.dp),
+                        fontSize = 16.sp
+                    )
+                    Text(
+                        text = (animeList[i]?.title?.romaji ?: "Null bro"),
+                        modifier = Modifier
+                            .padding(6.dp)
+                            .absolutePadding(left = 30.dp),
+                        fontSize = 16.sp
+                    )
+                    Text(
+                        text = (animeList[i]?.title?.native ?: "Null bro"),
+                        modifier = Modifier
+                            .padding(6.dp)
+                            .absolutePadding(left = 30.dp),
+                        fontSize = 16.sp
+                    )
+                }
+            }
+        }
+    }
 }
