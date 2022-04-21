21cbc5de62e804b2b302ace4c4afcdd5f23cdab2
imashnake0 <kamaleshpaluru07@gmail.com>
2022-04-21 16:20:55 -0400

    Added `@HiltAndroidApp` annotation
    Annotated Application class.

--- app/src/main/java/com/imashnake/animite/AnimiteApplication.kt
+++ app/src/main/java/com/imashnake/animite/AnimiteApplication.kt
@@ -2,10 +2,12 @@ package com.imashnake.animite
 
 import android.app.Application
 import com.imashnake.animite.data.Container
+import dagger.hilt.android.HiltAndroidApp
 
 /**
  * TODO: Kdoc.
  */
+@HiltAndroidApp
 class AnimiteApplication: Application() {
     val container = Container()
 }
