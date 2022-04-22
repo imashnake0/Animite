53c1881abe5688cf56c64189a31067531f04954b
imashnake0 <kamaleshpaluru07@gmail.com>
2022-04-21 20:16:39 -0400

    Injected network dependencies
    Created a `NetworkModule` and injected network dependencies.

--- app/src/main/java/com/imashnake/animite/data/repos/AnimeRepository.kt
+++ app/src/main/java/com/imashnake/animite/data/repos/AnimeRepository.kt
@@ -2,11 +2,12 @@ package com.imashnake.animite.data.repos
 
 import com.imashnake.animite.AnimeQuery
 import com.imashnake.animite.data.sauce.AnimeNetworkSource
+import javax.inject.Inject
 
 /**
  * TODO: Kdoc.
  */
-class AnimeRepository(
+class AnimeRepository @Inject constructor(
     private val animeNetworkSource: AnimeNetworkSource
 ) {
     suspend fun fetchAnime(id: Int): AnimeQuery.Media? =
--- app/src/main/java/com/imashnake/animite/data/sauce/AnimeNetworkSource.kt
+++ app/src/main/java/com/imashnake/animite/data/sauce/AnimeNetworkSource.kt
@@ -3,11 +3,12 @@ package com.imashnake.animite.data.sauce
 import com.imashnake.animite.AnimeQuery
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
 /**
  * TODO: Kdoc.
  */
-class AnimeNetworkSource(
+class AnimeNetworkSource @Inject constructor(
     private val animeApi: AnimeApi,
     private val dispatcher: CoroutineDispatcher
 ) {
