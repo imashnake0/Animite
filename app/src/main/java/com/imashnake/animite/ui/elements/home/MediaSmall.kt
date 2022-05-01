79e7bd9036d1b675809f48840c86af1d796b4093
imashnake0 <kamaleshpaluru07@gmail.com>
2022-05-01 00:48:22 -0400

    Fixed clipping
    Used `AsyncImage`'s clip API to change the shape.

--- app/src/main/java/com/imashnake/animite/ui/elements/home/MediaSmall.kt
+++ app/src/main/java/com/imashnake/animite/ui/elements/home/MediaSmall.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -38,14 +39,14 @@ fun MediaSmall(image: String?, anime: String?) {
         containerColor = Card,
         shape = mediaSmallShape
     ) {
-        Card(shape = mediaSmallShape) {
-            AsyncImage(
-                model = image,
-                contentDescription = anime,
-                contentScale = ContentScale.Crop,
-                modifier = Modifier.height(198.dp)
-            )
-        }
+        AsyncImage(
+            model = image,
+            contentDescription = anime,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .height(198.dp)
+                .clip(mediaSmallShape)
+        )
         Text(
             text = anime ?: "",
             color = Text,
