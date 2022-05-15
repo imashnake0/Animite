ed0755ae25d004f2d4bd2306aa40221b25d2a9a7
imashnake0 <kamaleshpaluru07@gmail.com>
2022-05-15 15:10:42 -0400

    Updated `compose-material3`
    Updated Compose Material3 and made changes to `Card()` according to release notes: https://developer.android.com/jetpack/androidx/releases/compose-material3#1.0.0-alpha10.

--- app/src/main/java/com/imashnake/animite/ui/elements/home/MediaSmall.kt
+++ app/src/main/java/com/imashnake/animite/ui/elements/home/MediaSmall.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults.cardColors
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -31,11 +32,10 @@ import com.imashnake.animite.ui.theme.mediaSmallShape
 @Composable
 fun MediaSmall(image: String?, anime: String?) {
     Card(
-        onClick = { },
         modifier = Modifier
             .wrapContentHeight()
             .width(115.dp),
-        containerColor = Card,
+        colors = cardColors(containerColor = Card),
         shape = mediaSmallShape
     ) {
         AsyncImage(
