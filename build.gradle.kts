726ee7b8115ea0e494429c879f3b0d09756e146d
imashnake0 <kamaleshpaluru07@gmail.com>
2022-06-01 23:47:49 -0400

    Renamed catalog to `libs`
    Please stop yelling at me Gradle-san.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 // TODO: Remove this after https://youtrack.jetbrains.com/issue/KTIJ-19369 is resolved.
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    alias(deps.plugins.agp) apply false
-    alias(deps.plugins.kotlin) apply false
-    alias(deps.plugins.hilt) apply false
+    alias(libs.plugins.agp) apply false
+    alias(libs.plugins.kotlin) apply false
+    alias(libs.plugins.hilt) apply false
 }
 
 tasks.register("clean", Delete::class) {
