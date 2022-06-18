bc5f8fb0ab8d22d2294d4f9f358a09ca4189f94e
Relwi <theofficialdork@hotmail.com>
2022-06-18 12:33:28 +0200

    Use LazyColumn in the SearchList

--- app/src/main/java/com/imashnake/animite/ui/elements/home/SearchBar.kt
+++ app/src/main/java/com/imashnake/animite/ui/elements/home/SearchBar.kt
@@ -2,6 +2,8 @@ package com.imashnake.animite.ui.elements.home
 
 import android.util.Log
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults
@@ -29,6 +31,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.imashnake.animite.R
+import com.imashnake.animite.SearchQuery
 import com.imashnake.animite.ui.state.SearchViewModel
 import com.imashnake.animite.ui.theme.NavigationBar
 import com.imashnake.animite.ui.theme.Text
@@ -126,30 +129,32 @@ fun SearchList(
     viewModel: SearchViewModel = viewModel(),
     modifier: Modifier
 ) {
-    // TODO: Use `LazyColumn` instead.
-    Column(modifier = modifier) {
-        val searchList = viewModel.uiState.searchList?.media
+    val searchList = viewModel.uiState.searchList?.media
 
-        if(searchList != null && searchList.isNotEmpty()) {
-            searchList.forEach {
-                Text(
-                    text = it?.title?.romaji ?:
-                        it?.title?.english ?:
-                        it?.title?.native ?: "",
-                    color = Text,
-                    fontSize = 12.sp,
-                    maxLines = 1,
-                    modifier = Modifier
-                        .padding(11.dp),
-                    fontFamily = manropeFamily,
-                    fontWeight = FontWeight.Medium
-                )
+    if (searchList != null && searchList.isNotEmpty()) {
+        LazyColumn(modifier = modifier) {
+            items(searchList) {
+                SearchItem(item = it)
             }
-        } else {
-            // TODO: Handle errors.
-            Log.d("bruh", "bruh")
         }
+    } else {
+        // TODO: Handle errors.
+        Log.d("bruh", "bruh")
     }
+}
+
+@Composable
+private fun SearchItem(item: SearchQuery.Medium?) {
+    Text(
+        text = item?.title?.romaji ?: item?.title?.english ?: item?.title?.native ?: "",
+        color = Text,
+        fontSize = 12.sp,
+        maxLines = 1,
+        modifier = Modifier
+            .padding(11.dp),
+        fontFamily = manropeFamily,
+        fontWeight = FontWeight.Medium
+    )
 }
 
 @ExperimentalComposeUiApi
