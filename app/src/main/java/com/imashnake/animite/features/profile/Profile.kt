fb9f6d771bf48184fddae45750916b83623a5208
imashnake0 <kamaleshpaluru07@gmail.com>
2022-09-11 15:14:52 -0400

    Themed `Profile` and `RSlash`

--- app/src/main/java/com/imashnake/animite/features/profile/Profile.kt
+++ app/src/main/java/com/imashnake/animite/features/profile/Profile.kt
@@ -6,10 +6,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.imashnake.animite.features.theme.AnimiteTheme
 import com.imashnake.animite.features.theme.Backdrop
 import com.imashnake.animite.features.theme.Text
 import com.ramcosta.composedestinations.annotation.Destination
@@ -19,15 +21,17 @@ import com.ramcosta.composedestinations.annotation.Destination
 @Destination(style = ProfileTransitions::class)
 @Composable
 fun Profile() {
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Backdrop)
-    ) {
-        CircularProgressIndicator(
-            color = Text,
-            strokeWidth = 8.dp
-        )
+    AnimiteTheme {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background)
+        ) {
+            CircularProgressIndicator(
+                color = MaterialTheme.colorScheme.onBackground,
+                strokeWidth = 8.dp
+            )
+        }
     }
 }
--- app/src/main/java/com/imashnake/animite/features/rslash/RSlash.kt
+++ app/src/main/java/com/imashnake/animite/features/rslash/RSlash.kt
@@ -4,10 +4,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.imashnake.animite.features.theme.AnimiteTheme
 import com.imashnake.animite.features.theme.Backdrop
 import com.imashnake.animite.features.theme.Text
 import com.ramcosta.composedestinations.annotation.Destination
@@ -16,15 +18,17 @@ import com.ramcosta.composedestinations.annotation.Destination
 @Destination
 @Composable
 fun RSlash() {
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Backdrop)
-    ) {
-        CircularProgressIndicator(
-            color = Text,
-            strokeWidth = 8.dp
-        )
+    AnimiteTheme {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background)
+        ) {
+            CircularProgressIndicator(
+                color = MaterialTheme.colorScheme.onBackground,
+                strokeWidth = 8.dp
+            )
+        }
     }
 }
