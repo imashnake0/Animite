d915be7a98aae3ebed9845ef159aeb7d53e419d2
imashnake0 <kamaleshpaluru07@gmail.com>
2022-09-14 20:39:17 -0400

    Added `ColorScheme` for < API 31

--- app/src/main/java/com/imashnake/animite/features/MainActivity.kt
+++ app/src/main/java/com/imashnake/animite/features/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.imashnake.animite.features
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.*
@@ -9,8 +8,10 @@ import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.ColorScheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.SideEffect
@@ -50,41 +51,6 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             AnimiteTheme {
-                Log.d(
-                    "ColorSchemeBlah",
-                    """
-                        ${MaterialTheme.colorScheme.primary}
-                        ${MaterialTheme.colorScheme.onPrimary}
-                        ${MaterialTheme.colorScheme.primaryContainer}
-                        ${MaterialTheme.colorScheme.onPrimaryContainer}
-                        ${MaterialTheme.colorScheme.inversePrimary}
-                        ${MaterialTheme.colorScheme.secondary}
-                        ${MaterialTheme.colorScheme.onSecondary}
-                        ${MaterialTheme.colorScheme.secondaryContainer}
-                        ${MaterialTheme.colorScheme.onSecondaryContainer}
-                        ${MaterialTheme.colorScheme.tertiary}
-                        ${MaterialTheme.colorScheme.onTertiary}
-                        ${MaterialTheme.colorScheme.tertiaryContainer}
-                        ${MaterialTheme.colorScheme.onTertiaryContainer}
-                        ${MaterialTheme.colorScheme.background}
-                        ${MaterialTheme.colorScheme.onBackground}
-                        ${MaterialTheme.colorScheme.surface}
-                        ${MaterialTheme.colorScheme.onSurface}
-                        ${MaterialTheme.colorScheme.surfaceVariant}
-                        ${MaterialTheme.colorScheme.onSurfaceVariant}
-                        ${MaterialTheme.colorScheme.surfaceTint}
-                        ${MaterialTheme.colorScheme.inverseSurface}
-                        ${MaterialTheme.colorScheme.inverseOnSurface}
-                        ${MaterialTheme.colorScheme.error}
-                        ${MaterialTheme.colorScheme.onError}
-                        ${MaterialTheme.colorScheme.errorContainer}
-                        ${MaterialTheme.colorScheme.onErrorContainer}
-                        ${MaterialTheme.colorScheme.outline}
-                        ${MaterialTheme.colorScheme.outlineVariant}
-                        ${MaterialTheme.colorScheme.scrim}
-                    """.trimIndent()
-                )
-
                 val systemUiController = rememberSystemUiController()
                 SideEffect {
                     systemUiController.setSystemBarsColor(Color.Transparent, darkIcons = false)
--- app/src/main/java/com/imashnake/animite/features/theme/Theme.kt
+++ app/src/main/java/com/imashnake/animite/features/theme/Theme.kt
@@ -1,18 +1,26 @@
 package com.imashnake.animite.features.theme
 
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 @Composable
 fun AnimiteTheme(content: @Composable () -> Unit) {
-    // TODO: Create a colorScheme for < API 31:
-    //  https://developer.android.com/jetpack/compose/themes/material#m3-dynamic-color.
-    val colorScheme = dynamicDarkColorScheme(LocalContext.current)
+    val dynamicColor = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+    val darkTheme = isSystemInDarkTheme()
+    val animiteColorScheme = when {
+        dynamicColor && darkTheme -> dynamicDarkColorScheme(LocalContext.current)
+        dynamicColor && !darkTheme -> dynamicLightColorScheme(LocalContext.current)
+        darkTheme -> KimiNoDarkColorScheme
+        else -> KimiNoLightColorScheme
+    }
 
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = animiteColorScheme,
         typography = AnimiteTypography,
         content = content
     )
