8f2ce8fcde8baa3d764ef58f128ea26764b1ca52
imashnake0 <kamaleshpaluru07@gmail.com>
2022-10-06 22:22:49 -0400

    Translucent status bar
    Made the status bar translucent on scrolling.

--- app/src/main/java/com/imashnake/animite/features/home/Home.kt
+++ app/src/main/java/com/imashnake/animite/features/home/Home.kt
@@ -6,16 +6,18 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.LinearProgressIndicator
@@ -29,6 +31,7 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -65,177 +68,194 @@ fun Home(
         upcomingNextSeasonMediaList != null &&
         allTimePopularMediaList != null -> {
             val scrollState = rememberScrollState()
-            Box(
-                modifier = Modifier
-                    .verticalScroll(scrollState)
-                    .navigationBarsPadding()
-            ) {
-                Box {
-                    Image(
-                        painter = painterResource(Res.drawable.background),
-                        contentDescription = null,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(dimensionResource(Res.dimen.banner_height))
-                            .graphicsLayer {
-                                translationY = 0.7f * scrollState.value
-                            },
-                        contentScale = ContentScale.Crop,
-                        alignment = Alignment.TopCenter
-                    )
+            Box {
+                Box(
+                    modifier = Modifier
+                        .verticalScroll(scrollState)
+                        .navigationBarsPadding()
+                ) {
+                    Box {
+                        Image(
+                            painter = painterResource(Res.drawable.background),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(dimensionResource(Res.dimen.banner_height))
+                                .graphicsLayer {
+                                    translationY = 0.7f * scrollState.value
+                                },
+                            contentScale = ContentScale.Crop,
+                            alignment = Alignment.TopCenter
+                        )
 
-                    Box(
-                        modifier = Modifier
-                            .background(
-                                Brush.verticalGradient(
-                                    listOf(
-                                        Color.Transparent,
-                                        MaterialTheme.colorScheme.secondaryContainer.copy(
-                                            alpha = 0.5f
+                        Box(
+                            modifier = Modifier
+                                .background(
+                                    Brush.verticalGradient(
+                                        listOf(
+                                            Color.Transparent,
+                                            MaterialTheme.colorScheme.secondaryContainer.copy(
+                                                alpha = 0.5f
+                                            )
                                         )
                                     )
                                 )
-                            )
-                            .fillMaxWidth()
-                            .height(dimensionResource(Res.dimen.banner_height))
-                    ) { }
-
-                    Text(
-                        text = stringResource(Res.string.okaeri),
-                        color = MaterialTheme.colorScheme.onSecondaryContainer,
-                        style = MaterialTheme.typography.displayMedium,
-                        modifier = Modifier
-                            .align(Alignment.BottomStart)
-                            .padding(
-                                start = dimensionResource(Res.dimen.large_padding),
-                                bottom = dimensionResource(Res.dimen.medium_padding)
-                            )
-                    )
-                }
-
-                // TODO: Use `padding` instead of the `Spacer`s.
-                Column {
-                    Spacer(Modifier.size(dimensionResource(Res.dimen.banner_height)))
-
-                    Column(
-                        modifier = Modifier.background(MaterialTheme.colorScheme.background)
-                    ) {
-                        Spacer(Modifier.size(dimensionResource(Res.dimen.large_padding)))
+                                .fillMaxWidth()
+                                .height(dimensionResource(Res.dimen.banner_height))
+                        ) { }
 
                         Text(
-                            text = stringResource(Res.string.trending_now),
-                            color = MaterialTheme.colorScheme.onBackground,
-                            style = MaterialTheme.typography.titleMedium,
-                            modifier = Modifier.padding(
-                                start = dimensionResource(Res.dimen.large_padding)
-                            )
-                        )
-
-                        Spacer(Modifier.size(dimensionResource(Res.dimen.medium_padding)))
-
-                        MediaSmallRow(
-                            mediaList = trendingNowMediaList,
-                            onItemClick = { itemId ->
-                                navigator.navigate(
-                                    MediaPageDestination(
-                                        id = itemId,
-                                        mediaTypeArg = homeMediaType.rawValue
-                                    )
-                                ) {
-                                    launchSingleTop = true
-                                }
-                            }
-                        )
-
-                        Spacer(Modifier.size(dimensionResource(Res.dimen.large_padding)))
-
-                        Text(
-                            text = stringResource(Res.string.popular_this_season),
-                            color = MaterialTheme.colorScheme.onBackground,
-                            style = MaterialTheme.typography.titleMedium,
-                            modifier = Modifier.padding(
-                                start = dimensionResource(Res.dimen.large_padding)
-                            )
-                        )
-
-                        Spacer(Modifier.size(dimensionResource(Res.dimen.medium_padding)))
-
-                        MediaSmallRow(
-                            mediaList = popularThisSeasonMediaList,
-                            onItemClick = { itemId ->
-                                navigator.navigate(
-                                    MediaPageDestination(
-                                        id = itemId,
-                                        mediaTypeArg = homeMediaType.rawValue
-                                    )
-                                ) {
-                                    launchSingleTop = true
-                                }
-                            }
-                        )
-
-                        Spacer(Modifier.size(dimensionResource(Res.dimen.large_padding)))
-
-                        Text(
-                            text = stringResource(Res.string.upcoming_next_season),
-                            color = MaterialTheme.colorScheme.onBackground,
-                            style = MaterialTheme.typography.titleMedium,
-                            modifier = Modifier.padding(
-                                start = dimensionResource(Res.dimen.large_padding)
-                            )
-                        )
-
-                        Spacer(Modifier.size(dimensionResource(Res.dimen.medium_padding)))
-
-                        MediaSmallRow(
-                            mediaList = upcomingNextSeasonMediaList,
-                            onItemClick = { itemId ->
-                                navigator.navigate(
-                                    MediaPageDestination(
-                                        id = itemId,
-                                        mediaTypeArg = homeMediaType.rawValue
-                                    )
-                                ) {
-                                    launchSingleTop = true
-                                }
-                            }
-                        )
-
-                        Spacer(Modifier.size(dimensionResource(Res.dimen.large_padding)))
-
-                        Text(
-                            text = stringResource(Res.string.all_time_popular),
-                            color = MaterialTheme.colorScheme.onBackground,
-                            style = MaterialTheme.typography.titleMedium,
-                            modifier = Modifier.padding(
-                                start = dimensionResource(Res.dimen.large_padding)
-                            )
-                        )
-
-                        Spacer(Modifier.size(dimensionResource(Res.dimen.medium_padding)))
-
-                        MediaSmallRow(
-                            mediaList = allTimePopularMediaList,
-                            onItemClick = { itemId ->
-                                navigator.navigate(
-                                    MediaPageDestination(
-                                        id = itemId,
-                                        mediaTypeArg = homeMediaType.rawValue
-                                    )
-                                ) {
-                                    launchSingleTop = true
-                                }
-                            }
-                        )
-
-                        Spacer(
-                            Modifier.height(
-                                dimensionResource(Res.dimen.navigation_bar_height)
-                                        + dimensionResource(Res.dimen.large_padding)
-                            )
+                            text = stringResource(Res.string.okaeri),
+                            color = MaterialTheme.colorScheme.onSecondaryContainer,
+                            style = MaterialTheme.typography.displayMedium,
+                            modifier = Modifier
+                                .align(Alignment.BottomStart)
+                                .padding(
+                                    start = dimensionResource(Res.dimen.large_padding),
+                                    bottom = dimensionResource(Res.dimen.medium_padding)
+                                )
                         )
                     }
+
+                    // TODO: Use `padding` instead of the `Spacer`s.
+                    Column {
+                        Spacer(Modifier.size(dimensionResource(Res.dimen.banner_height)))
+
+                        Column(
+                            modifier = Modifier.background(MaterialTheme.colorScheme.background)
+                        ) {
+                            Spacer(Modifier.size(dimensionResource(Res.dimen.large_padding)))
+
+                            Text(
+                                text = stringResource(Res.string.trending_now),
+                                color = MaterialTheme.colorScheme.onBackground,
+                                style = MaterialTheme.typography.titleMedium,
+                                modifier = Modifier.padding(
+                                    start = dimensionResource(Res.dimen.large_padding)
+                                )
+                            )
+
+                            Spacer(Modifier.size(dimensionResource(Res.dimen.medium_padding)))
+
+                            MediaSmallRow(
+                                mediaList = trendingNowMediaList,
+                                onItemClick = { itemId ->
+                                    navigator.navigate(
+                                        MediaPageDestination(
+                                            id = itemId,
+                                            mediaTypeArg = homeMediaType.rawValue
+                                        )
+                                    ) {
+                                        launchSingleTop = true
+                                    }
+                                }
+                            )
+
+                            Spacer(Modifier.size(dimensionResource(Res.dimen.large_padding)))
+
+                            Text(
+                                text = stringResource(Res.string.popular_this_season),
+                                color = MaterialTheme.colorScheme.onBackground,
+                                style = MaterialTheme.typography.titleMedium,
+                                modifier = Modifier.padding(
+                                    start = dimensionResource(Res.dimen.large_padding)
+                                )
+                            )
+
+                            Spacer(Modifier.size(dimensionResource(Res.dimen.medium_padding)))
+
+                            MediaSmallRow(
+                                mediaList = popularThisSeasonMediaList,
+                                onItemClick = { itemId ->
+                                    navigator.navigate(
+                                        MediaPageDestination(
+                                            id = itemId,
+                                            mediaTypeArg = homeMediaType.rawValue
+                                        )
+                                    ) {
+                                        launchSingleTop = true
+                                    }
+                                }
+                            )
+
+                            Spacer(Modifier.size(dimensionResource(Res.dimen.large_padding)))
+
+                            Text(
+                                text = stringResource(Res.string.upcoming_next_season),
+                                color = MaterialTheme.colorScheme.onBackground,
+                                style = MaterialTheme.typography.titleMedium,
+                                modifier = Modifier.padding(
+                                    start = dimensionResource(Res.dimen.large_padding)
+                                )
+                            )
+
+                            Spacer(Modifier.size(dimensionResource(Res.dimen.medium_padding)))
+
+                            MediaSmallRow(
+                                mediaList = upcomingNextSeasonMediaList,
+                                onItemClick = { itemId ->
+                                    navigator.navigate(
+                                        MediaPageDestination(
+                                            id = itemId,
+                                            mediaTypeArg = homeMediaType.rawValue
+                                        )
+                                    ) {
+                                        launchSingleTop = true
+                                    }
+                                }
+                            )
+
+                            Spacer(Modifier.size(dimensionResource(Res.dimen.large_padding)))
+
+                            Text(
+                                text = stringResource(Res.string.all_time_popular),
+                                color = MaterialTheme.colorScheme.onBackground,
+                                style = MaterialTheme.typography.titleMedium,
+                                modifier = Modifier.padding(
+                                    start = dimensionResource(Res.dimen.large_padding)
+                                )
+                            )
+
+                            Spacer(Modifier.size(dimensionResource(Res.dimen.medium_padding)))
+
+                            MediaSmallRow(
+                                mediaList = allTimePopularMediaList,
+                                onItemClick = { itemId ->
+                                    navigator.navigate(
+                                        MediaPageDestination(
+                                            id = itemId,
+                                            mediaTypeArg = homeMediaType.rawValue
+                                        )
+                                    ) {
+                                        launchSingleTop = true
+                                    }
+                                }
+                            )
+
+                            Spacer(
+                                Modifier.height(
+                                    dimensionResource(Res.dimen.navigation_bar_height)
+                                            + dimensionResource(Res.dimen.large_padding)
+                                )
+                            )
+                        }
+                    }
                 }
+
+                // Translucent status bar.
+                val bannerHeight = with(LocalDensity.current) { dimensionResource(Res.dimen.banner_height).toPx() }
+                Box(
+                    modifier = Modifier
+                        .graphicsLayer {
+                            alpha = 0.75f * if (scrollState.value < bannerHeight) {
+                                    scrollState.value.toFloat()/bannerHeight
+                                } else 1f
+                        }
+                        .background(color = MaterialTheme.colorScheme.background)
+                        .fillMaxWidth()
+                        .height(WindowInsets.statusBars.asPaddingValues().calculateTopPadding())
+                        .align(Alignment.TopCenter)
+                ) {  }
             }
         }
 
