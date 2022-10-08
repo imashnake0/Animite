8395ab5a1cbd7518cf2220f6cc065fd02f2b10df
imashnake0 <kamaleshpaluru07@gmail.com>
2022-10-08 02:50:26 -0400

    Cleanup

--- app/src/main/java/com/imashnake/animite/features/home/MediaSmall.kt
+++ app/src/main/java/com/imashnake/animite/features/home/MediaSmall.kt
@@ -2,7 +2,6 @@ package com.imashnake.animite.features.home
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
