d9c063d41cc01cc588f82786bcfcaae6c727277e
imashnake0 <kamaleshpaluru07@gmail.com>
2022-10-13 00:45:47 -0400

    Updated refreshVersions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,13 +5,11 @@ pluginManagement {
         gradlePluginPortal()
         google()
         mavenCentral()
-        maven("https://s01.oss.sonatype.org/content/repositories/snapshots")
     }
 }
 
 plugins {
-    // TODO: This is for testing https://github.com/jmfayard/refreshVersions/pull/509.
-    id("de.fayard.refreshVersions") version "0.41.0-SNAPSHOT"
+    id("de.fayard.refreshVersions") version "0.50.2"
 }
 
 dependencyResolutionManagement {
