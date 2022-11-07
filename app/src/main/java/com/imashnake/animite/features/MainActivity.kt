122d490c54db94258dd4bfe45046ee9f11e1dbcc
Jack Boswell (boswelja) <boswelja@outlook.com>
2022-11-07 22:43:54 +1300

    Move content somewhere we can navigate from

--- app/src/main/java/com/imashnake/animite/features/MainActivity.kt
+++ app/src/main/java/com/imashnake/animite/features/MainActivity.kt
@@ -4,33 +4,12 @@ import android.os.Build
 import android.os.Bundle
 import android.view.WindowManager
 import androidx.activity.compose.setContent
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.slideOutVertically
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.navigationBarsPadding
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
 import com.bumble.appyx.core.integration.NodeHost
 import com.bumble.appyx.core.integrationpoint.NodeActivity
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
-import com.imashnake.animite.R
-import com.imashnake.animite.features.searchbar.SearchBar
-import com.imashnake.animite.features.theme.AnimiteTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -46,53 +25,13 @@ class MainActivity : NodeActivity() {
         }
 
         setContent {
-            AnimiteTheme {
-                val systemUiController = rememberSystemUiController()
-                SideEffect {
-                    systemUiController.setSystemBarsColor(Color.Transparent, darkIcons = false)
-                }
+            val systemUiController = rememberSystemUiController()
+            SideEffect {
+                systemUiController.setSystemBarsColor(Color.Transparent, darkIcons = false)
+            }
 
-                Box(Modifier.fillMaxSize()) {
-//                    DestinationsNavHost(
-//                        navGraph = NavGraphs.root,
-//                        modifier = Modifier
-//                            .align(Alignment.TopCenter)
-//                            .background(MaterialTheme.colorScheme.background)
-//                            .fillMaxSize(),
-//                        navController = navController,
-//                        engine = navHostEngine
-//                    )
-                    NodeHost(integrationPoint = appyxIntegrationPoint) {
-                        RootNode(it)
-                    }
-
-                    val searchBarBottomPadding: Dp by animateDpAsState(
-                        targetValue = dimensionResource(R.dimen.large_padding) + if (
-                            true
-                        ) dimensionResource(R.dimen.navigation_bar_height) else 0.dp
-                    )
-
-                    SearchBar(
-                        onItemClicked = { /* TODO */ },
-                        modifier = Modifier
-                            .align(Alignment.BottomEnd)
-                            .padding(
-                                start = dimensionResource(R.dimen.large_padding),
-                                end = dimensionResource(R.dimen.large_padding),
-                                bottom = searchBarBottomPadding
-                            )
-                            .navigationBarsPadding()
-                    )
-
-                    AnimatedVisibility(
-                        visible = true,
-                        modifier = Modifier.align(Alignment.BottomCenter),
-                        enter = slideInVertically { it },
-                        exit = slideOutVertically { it }
-                    ) {
-                        // NavigationBar(navController = navController)
-                    }
-                }
+            NodeHost(integrationPoint = appyxIntegrationPoint) {
+                RootNode(it)
             }
         }
     }
--- app/src/main/java/com/imashnake/animite/features/RootNode.kt
+++ app/src/main/java/com/imashnake/animite/features/RootNode.kt
@@ -1,17 +1,45 @@
 package com.imashnake.animite.features
 
 import android.os.Parcelable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.add
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.mandatorySystemGestures
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.AccountCircle
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.bumble.appyx.core.composable.Children
+import com.bumble.appyx.core.composable.childrenAsState
 import com.bumble.appyx.core.modality.BuildContext
 import com.bumble.appyx.core.node.Node
 import com.bumble.appyx.core.node.ParentNode
 import com.bumble.appyx.core.node.node
 import com.bumble.appyx.navmodel.backstack.BackStack
+import com.bumble.appyx.navmodel.backstack.operation.newRoot
+import com.imashnake.animite.R
 import com.imashnake.animite.features.home.Home
 import com.imashnake.animite.features.profile.Profile
 import com.imashnake.animite.features.rslash.RSlash
+import com.imashnake.animite.features.searchbar.SearchBar
+import com.imashnake.animite.features.theme.AnimiteTheme
 import kotlinx.parcelize.Parcelize
 
 class RootNode(
@@ -35,7 +63,60 @@ class RootNode(
 
     @Composable
     override fun View(modifier: Modifier) {
-        Children(navModel = backStack)
+        val screenState by backStack.childrenAsState()
+
+        AnimiteTheme {
+            // TODO Scaffold
+            Column(modifier.background(MaterialTheme.colorScheme.background)) {
+                Box(Modifier.fillMaxWidth().weight(1f)) {
+                    Children(navModel = backStack)
+
+                    SearchBar(
+                        onItemClicked = { /* TODO */ },
+                        modifier = Modifier
+                            .align(Alignment.BottomEnd)
+                            .padding(
+                                start = dimensionResource(R.dimen.large_padding),
+                                end = dimensionResource(R.dimen.large_padding),
+                                bottom = dimensionResource(R.dimen.large_padding)
+                            )
+                            .navigationBarsPadding()
+                    )
+                }
+                NavigationBar(
+                    modifier = Modifier.fillMaxWidth(),
+                    windowInsets = WindowInsets.navigationBars.add(WindowInsets.mandatorySystemGestures)
+                ) {
+                    NavigationBarItem(
+                        selected = screenState.last().key.navTarget == NavTarget.RSlash,
+                        onClick = {
+                            backStack.newRoot(NavTarget.RSlash)
+                        },
+                        icon = {
+                            Icon(painterResource(R.drawable.rslash), stringResource(R.string.rslash))
+                        }
+                    )
+                    NavigationBarItem(
+                        selected = screenState.last().key.navTarget == NavTarget.Home,
+                        onClick = {
+                            backStack.newRoot(NavTarget.Home)
+                        },
+                        icon = {
+                            Icon(painterResource(R.drawable.home), stringResource(R.string.home))
+                        }
+                    )
+                    NavigationBarItem(
+                        selected = screenState.last().key.navTarget == NavTarget.Profile,
+                        onClick = {
+                            backStack.newRoot(NavTarget.Profile)
+                        },
+                        icon = {
+                            Icon(Icons.Rounded.AccountCircle, stringResource(R.string.profile))
+                        }
+                    )
+                }
+            }
+        }
     }
 
     sealed class NavTarget : Parcelable {
