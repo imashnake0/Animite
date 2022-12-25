9877801b2aa981a73e835440a2663e2331c75237
Ian Roberts <ian.roberts@vet-ai.com>
2022-12-25 21:12:47 +0000

    Sorry Jack

--- app/src/main/java/com/imashnake/animite/data/sauce/NetworkBoundResource.kt
+++ app/src/main/java/com/imashnake/animite/data/sauce/NetworkBoundResource.kt
@@ -6,9 +6,9 @@ import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
 
 // TODO improve the handling of this, it's a bit too basic
-fun <T> Flow<T>.asState(): Flow<Resource<T>> {
+fun <T> Flow<T>.asState(): Flow<Resource<T?>> {
     return map {
-        Resource.success(it)
+        Resource.success<T?>(it)
     }.catch { exception ->
         emit(Resource.error(exception.message ?: "$exception"))
     }
