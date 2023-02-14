228a37650c4e140d2dc395c9df13430ae1e98da4
Jack Boswell (boswelja) <boswelja@outlook.com>
2023-02-14 19:31:04 +1300

    Update exception handling logic

--- api/anilist/src/main/kotlin/com/imashnake/animite/api/anilist/FlowExt.kt
+++ api/anilist/src/main/kotlin/com/imashnake/animite/api/anilist/FlowExt.kt
@@ -4,16 +4,25 @@ import com.apollographql.apollo3.api.ApolloResponse
 import com.apollographql.apollo3.api.Operation
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.mapLatest
 import java.io.IOException
 
 @OptIn(ExperimentalCoroutinesApi::class)
 fun <T: Operation.Data, R> Flow<ApolloResponse<T>>.asResult(transform: (T) -> R): Flow<Result<R>> = this
-    .mapLatest {
-        // TODO Is there a better way to handle errors here?
-        it.data?.let { data ->
-            Result.success(transform(data))
-        } ?: Result.failure(IOException(it.errors?.firstOrNull()?.message.orEmpty()))
+    .mapLatest { response ->
+        if (response.data != null) {
+            Result.success(transform(response.dataAssertNoErrors))
+        } else if (response.hasErrors()) {
+            // TODO What kind of exception do we provide here?
+            throw IOException(response.errors!!.joinToString())
+        } else {
+            // No data, but also no exception. That's illegal
+            error("Unknown error occurred, no data or errors received.")
+        }
+    }
+    .catch { exception ->
+        emit(Result.failure(exception))
     }
 
 fun <T: Operation.Data> Flow<ApolloResponse<T>>.asResult(): Flow<Result<T>> = this.asResult { it }
