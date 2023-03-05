3c63eb3f5a3c8eadaf7163bbd5e1369173b3871a
imashnake0 <kamaleshpaluru07@gmail.com>
2023-03-05 17:20:51 -0500

    Added `MediaThemeUtil`

---
+++ app/src/main/java/com/imashnake/animite/features/media/MediaThemeUtil.kt
@@ -0,0 +1,37 @@
+package com.imashnake.animite.features.media
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import com.imashnake.animite.dev.ext.from
+import utils.ThemeUtils
+
+/**
+ * Remembers a new Material3 [ColorScheme] based on the given color, or falls back to the default
+ * color scheme.
+ *
+ * @param color The color to create and remember a color scheme for.
+ * @param fallbackColorScheme The color scheme to fall back to if [color] is null.
+ * @param isDark Whether the new color scheme is to be used in dark mode.
+ */
+@Composable
+fun rememberColorSchemeFor(
+    color: Int?,
+    fallbackColorScheme: ColorScheme = MaterialTheme.colorScheme,
+    isDark: Boolean = isSystemInDarkTheme()
+): ColorScheme {
+    return remember(color, isDark) {
+        if (color != null) {
+            fallbackColorScheme.from(
+                if (isDark)
+                    ThemeUtils.themeFromSourceColor(color).schemes.dark
+                else
+                    ThemeUtils.themeFromSourceColor(color).schemes.light
+            )
+        } else {
+            fallbackColorScheme
+        }
+    }
+}
