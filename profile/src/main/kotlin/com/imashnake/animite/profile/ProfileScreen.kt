c0645a89fb723467e12aab4ead5c1009b6a14f56
imashnake0 <kamaleshpaluru07@gmail.com>
2023-10-12 23:18:48 -0400

    Fun w `Canvas`

--- profile/src/main/kotlin/com/imashnake/animite/profile/ProfileScreen.kt
+++ profile/src/main/kotlin/com/imashnake/animite/profile/ProfileScreen.kt
@@ -1,9 +1,17 @@
 package com.imashnake.animite.profile
 
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,14 +20,22 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.rotate
+import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.zIndex
 import com.imashnake.animite.core.extensions.landscapeCutoutPadding
 import com.imashnake.animite.core.R as coreR
 import com.ramcosta.composedestinations.annotation.Destination
@@ -29,7 +45,7 @@ import com.ramcosta.composedestinations.annotation.RootNavGraph
 @Destination(route = "profile-screen")
 @Composable
 fun ProfileScreen() {
-    Box(
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.background)
@@ -62,7 +78,9 @@ fun ProfileScreen() {
             ) { }
 
             Row(
-                modifier = Modifier.fillMaxWidth().align(Alignment.BottomCenter),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.BottomCenter),
                 horizontalArrangement = Arrangement.SpaceBetween,
                 verticalAlignment = Alignment.CenterVertically
             ) {
@@ -80,6 +98,112 @@ fun ProfileScreen() {
                     maxLines = 1
                 )
             }
+        }
+
+        val orbitColor = MaterialTheme.colorScheme.onSecondaryContainer
+        val infiniteTransition = rememberInfiniteTransition(label = "revolutions")
+        val angle by infiniteTransition.animateFloat(
+            initialValue = 0f,
+            targetValue = 360f,
+            animationSpec = infiniteRepeatable(
+                animation = tween(10000, easing = LinearEasing),
+                repeatMode = RepeatMode.Restart
+            ),
+            label = "revolve_circle"
+        )
+
+        Canvas(
+            modifier = Modifier
+                .fillMaxSize()
+                .zIndex(-1f)
+        ) {
+            val pivot = this.center + Offset(this.size.width/2, -this.size.height/3)
+            val roundedRectangleSize = size.minDimension/7
+
+            translate(
+                left = this.size.width - roundedRectangleSize,
+                top = this.center.y - this.size.height/3 - roundedRectangleSize
+            ) {
+                rotate(angle, pivot = Offset(roundedRectangleSize, roundedRectangleSize)) {
+                    drawRoundRect(
+                        color = orbitColor.copy(alpha = 0.9f),
+                        size = Size(roundedRectangleSize*2, roundedRectangleSize*2),
+                        cornerRadius = CornerRadius(
+                            roundedRectangleSize/1.5f,
+                            roundedRectangleSize/1.5f
+                        )
+                    )
+                }
+            }
+
+            rotate(
+                degrees = angle,
+                pivot = pivot
+            ) {
+                translate(
+                    left = this.size.width - roundedRectangleSize/2,
+                    top = this.center.y - this.size.height/3 - roundedRectangleSize/2 + size.minDimension/2
+                ) {
+                    rotate(angle, pivot = Offset(roundedRectangleSize/2, roundedRectangleSize/2)) {
+                        drawRoundRect(
+                            color = orbitColor.copy(alpha = 0.7f),
+                            size = Size(roundedRectangleSize, roundedRectangleSize),
+                            cornerRadius = CornerRadius(
+                                roundedRectangleSize/4,
+                                roundedRectangleSize/4
+                            )
+                        )
+                    }
+                }
+
+                translate(
+                    left = this.size.width - roundedRectangleSize/2,
+                    top = this.center.y - this.size.height/3 - roundedRectangleSize/2 - size.minDimension/2
+                ) {
+                    rotate(-2.5f*angle, pivot = Offset(roundedRectangleSize/2, roundedRectangleSize/2)) {
+                        drawRoundRect(
+                            color = orbitColor.copy(alpha = 0.4f),
+                            size = Size(roundedRectangleSize, roundedRectangleSize),
+                            cornerRadius = CornerRadius(
+                                roundedRectangleSize/4,
+                                roundedRectangleSize/4
+                            )
+                        )
+                    }
+                }
+
+                translate(left = size.minDimension/2) {
+                    drawCircle(
+                        radius = size.minDimension/12,
+                        color = orbitColor.copy(alpha = 0.2f),
+                        center = pivot
+                    )
+                }
+
+                translate(left = -size.minDimension/2) {
+                    drawCircle(
+                        radius = size.minDimension/17,
+                        color = orbitColor.copy(alpha = 0.5f),
+                        center = pivot
+                    )
+                }
+
+                translate(top = roundedRectangleSize/4) {
+                    drawCircle(
+                        radius = size.minDimension/3,
+                        color = orbitColor.copy(alpha = 0.2f),
+                        center = this.center + Offset(this.size.width/2, -this.size.height/3),
+                        style = Stroke(width = 5f)
+                    )
+                }
+            }
+
+            drawCircle(
+                radius = size.minDimension/2,
+                color = orbitColor,
+                center = this.center + Offset(this.size.width/2, -this.size.height/3),
+                style = Stroke(width = 5f)
+            )
         }
     }
 }
