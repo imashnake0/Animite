80a32973150d072c3f6b104b89c8cad2dc0db708
imashnake0 <kamaleshpaluru07@gmail.com>
2024-01-03 02:17:21 -0500

    Cleanup

--- core/src/main/kotlin/com/imashnake/animite/core/ui/ProgressIndicator.kt
+++ core/src/main/kotlin/com/imashnake/animite/core/ui/ProgressIndicator.kt
@@ -2,12 +2,9 @@ package com.imashnake.animite.core.ui
 
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.LinearProgressIndicator
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.res.dimensionResource
 import com.imashnake.animite.core.R
