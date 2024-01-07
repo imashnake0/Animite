ca02677d5085a722525a22927cfa61b961e2a32c
Kamalesh Reddy Paluru <kamaleshpaluru07@gmail.com>
2024-01-07 17:05:08 -0500

    Cleanup

--- profile/src/main/kotlin/com/imashnake/animite/profile/ProfileViewModel.kt
+++ profile/src/main/kotlin/com/imashnake/animite/profile/ProfileViewModel.kt
@@ -9,9 +9,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.single
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
