c45663f4ad352bbcf083c3da2c480fe500469fe4
imashnake0 <krpaluru@uwaterloo.ca>
2024-02-18 04:07:29 -0500

    Revert "Stole `compose-markdown`"
    This reverts commit 03252a73119b3173e2f6f491f3b738213f9cb3eb.

--- profile/build.gradle.kts
+++ profile/build.gradle.kts
@@ -37,7 +37,6 @@ dependencies {
     implementation(projects.core)
     implementation(projects.api.anilist)
     implementation(projects.api.preferences)
-    implementation(projects.composeMarkdown)
 
     // AndroidX
     implementation(libs.androidx.activityCompose)
