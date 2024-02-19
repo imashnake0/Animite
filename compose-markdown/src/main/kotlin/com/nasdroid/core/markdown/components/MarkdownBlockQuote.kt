ce0b7bd3d5f49fafb14c0ef03bb87811bfc36dec
imashnake0 <krpaluru@uwaterloo.ca>
2024-02-19 02:17:45 -0500

    Cleanup

--- compose-markdown/src/main/kotlin/com/nasdroid/core/markdown/components/MarkdownBlockQuote.kt
+++ compose-markdown/src/main/kotlin/com/nasdroid/core/markdown/components/MarkdownBlockQuote.kt
@@ -9,13 +9,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.nasdroid.core.markdown.MarkdownNode
 import com.nasdroid.core.markdown.generator.MarkdownBlockQuote
