33a536a98770fb50c77076022d93d97774281497
imashnake0 <krpaluru@uwaterloo.ca>
2024-02-27 00:33:16 -0500

    Cleanup

--- app/src/main/java/com/imashnake/animite/features/ui/MediaSmall.kt
+++ app/src/main/java/com/imashnake/animite/features/ui/MediaSmall.kt
@@ -24,7 +24,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
@@ -32,11 +31,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
-import coil.request.ImageRequest
+import com.imashnake.animite.R
 import com.imashnake.animite.api.anilist.MediaListQuery
 import com.imashnake.animite.api.anilist.type.MediaType
-import com.imashnake.animite.core.Constants.CROSSFADE_DURATION
-import com.imashnake.animite.R
 import com.imashnake.animite.core.extensions.crossfadeModel
 import com.imashnake.animite.core.ui.LocalPaddings
 
