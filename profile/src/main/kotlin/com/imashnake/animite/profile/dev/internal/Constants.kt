4d31a4b5c616d8ed4407d08b426a3385b88118f1
imashnake0 <krpaluru@uwaterloo.ca>
2024-03-06 11:47:27 -0500

    Update Deeplink

--- profile/src/main/kotlin/com/imashnake/animite/profile/dev/internal/Constants.kt
+++ profile/src/main/kotlin/com/imashnake/animite/profile/dev/internal/Constants.kt
@@ -1,4 +1,4 @@
 package com.imashnake.animite.profile.dev.internal
 
-const val ANILIST_AUTH_DEEPLINK = "jinnah://animite#access_token={accessToken}&token_type=Bearer&expires_in=31622400"
-const val ANILIST_AUTH_URL = "https://anilist.co/api/v2/oauth/authorize?client_id=10678&response_type=token"
+const val ANILIST_AUTH_DEEPLINK = "app://animite#access_token={accessToken}&token_type=Bearer&expires_in=31622400"
+const val ANILIST_AUTH_URL = "https://anilist.co/api/v2/oauth/authorize?client_id=17347&response_type=token"
