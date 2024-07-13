5849683e72d97379b617fd9b00615bf086f0d3b7
imashnake0 <krpaluru@uwaterloo.ca>
2024-07-13 11:53:00 -0400

    Made Anime tab scrollable

--- profile/src/main/kotlin/com/imashnake/animite/profile/tabs/Anime.kt
+++ profile/src/main/kotlin/com/imashnake/animite/profile/tabs/Anime.kt
@@ -2,7 +2,10 @@ package com.imashnake.animite.profile.tabs
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
@@ -17,9 +20,17 @@ fun AnimeTab(
     mediaCollection: User.MediaCollection?,
     modifier: Modifier = Modifier
 ) {
-    // TODO: Why is this not smart-casting?
-    if (!mediaCollection?.namedLists.isNullOrEmpty()) {
-        UserMediaList(mediaCollection!!.namedLists, modifier)
+    val scrollState = rememberScrollState()
+
+    Column(
+        modifier
+            .verticalScroll(scrollState)
+            .padding(vertical = LocalPaddings.current.large)
+    ) {
+        // TODO: Why is this not smart-casting?
+        if (!mediaCollection?.namedLists.isNullOrEmpty()) {
+            UserMediaList(mediaCollection!!.namedLists, modifier)
+        }
     }
 }
 
