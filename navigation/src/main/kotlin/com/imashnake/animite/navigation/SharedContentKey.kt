7362748264daa9b2c506f2c009f42eb4b14a3284
imashnake0 <krpaluru@uwaterloo.ca>
2024-12-02 02:28:57 -0500

    Moved `SharedContentKey`

--- navigation/src/main/kotlin/com/imashnake/animite/navigation/SharedContentKey.kt
+++ navigation/src/main/kotlin/com/imashnake/animite/navigation/SharedContentKey.kt
@@ -1,5 +1,11 @@
-package com.imashnake.animite.dev
+package com.imashnake.animite.navigation
 
+/**
+ * Builds a key via [toString] for shared element transitions from [Component] to [Component].
+ *
+ * @see [androidx.compose.animation.SharedTransitionScope.rememberSharedContentState]
+ * @see [androidx.compose.animation.SharedTransitionScope.SharedContentState.key]
+ */
 data class SharedContentKey(
     val id: Int? = null,
     val source: String? = null,
