7c51fcf96f4c7db7a35d7dd934da3b5ec7597133
imashnake0 <krpaluru@uwaterloo.ca>
2024-12-29 02:00:52 -0500

    Typo

--- api/anilist/src/main/kotlin/com/imashnake/animite/api/anilist/sanitize/media/MediaList.kt
+++ api/anilist/src/main/kotlin/com/imashnake/animite/api/anilist/sanitize/media/MediaList.kt
@@ -5,7 +5,7 @@ data class MediaList(
     val list: List<Media.Small>
 ) {
     enum class Type(val title: String? = null) {
-        TRENDING_NOW("Tending Now"),
+        TRENDING_NOW("Trending Now"),
         POPULAR_THIS_SEASON("Popular This Season"),
         UPCOMING_NEXT_SEASON("Upcoming Next Season"),
         ALL_TIME_POPULAR("All Time Popular"),
