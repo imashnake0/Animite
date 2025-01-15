46b160f7c83295b088eb9560ecb771bb942a0186
imashnake0 <krpaluru@uwaterloo.ca>
2025-01-15 01:13:44 -0500

    Remove import

--- media/src/main/kotlin/com/imashnake/animite/media/MediaPage.kt
+++ media/src/main/kotlin/com/imashnake/animite/media/MediaPage.kt
@@ -15,7 +15,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.aspectRatio
