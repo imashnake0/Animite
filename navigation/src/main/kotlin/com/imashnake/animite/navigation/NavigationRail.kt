fd5446efd57fcbeda4925ca235d738055dbd0c47
imashnake0 <krpaluru@uwaterloo.ca>
2025-02-03 01:43:50 -0500

    Create `NavigationRail`

--- navigation/src/main/kotlin/com/imashnake/animite/navigation/NavigationRail.kt
+++ navigation/src/main/kotlin/com/imashnake/animite/navigation/NavigationRail.kt
@@ -1,47 +1,46 @@
 package com.imashnake.animite.navigation
 
-import android.content.res.Configuration
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.add
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.displayCutout
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.navigationBarsPadding
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
+import androidx.compose.foundation.layout.only
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.NavigationRailItem
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.compose.material3.NavigationRail as M3NavigationRail
 
 @Composable
-fun NavigationBar(
+fun NavigationRail(
     navController: NavController,
     modifier: Modifier = Modifier,
 ) {
     val currentBackStackEntry by navController.currentBackStackEntryAsState()
-    NavigationBar(
-        modifier = modifier.height(
-            dimensionResource(R.dimen.navigation_bar_height) + WindowInsets
-                .navigationBars
+    // TODO: This uses `background` as `containerColor`, I have no idea why.
+    M3NavigationRail(
+        modifier = modifier.width(
+            dimensionResource(R.dimen.navigation_rail_width) + WindowInsets
+                .displayCutout
                 .asPaddingValues()
-                .calculateBottomPadding()
+                .calculateLeftPadding(LayoutDirection.Ltr)
         ),
-        // TODO: Remove this after adding the rail.
-        windowInsets = if (LocalConfiguration.current.orientation
-            == Configuration.ORIENTATION_LANDSCAPE
-        ) { WindowInsets.displayCutout } else { WindowInsets(0.dp) }
+        windowInsets = WindowInsets.displayCutout.only(WindowInsetsSides.Left).add(WindowInsets.statusBars)
     ) {
         NavigationBarPaths.entries.forEach { destination ->
             val selected = remember(destination, currentBackStackEntry) {
                 currentBackStackEntry?.let { destination.matchesDestination(it) } ?: false
             }
-            NavigationBarItem(
+            NavigationRailItem(
                 selected = selected,
                 onClick = { if (!selected) destination.navigateTo(navController) },
                 icon = destination.icon,
