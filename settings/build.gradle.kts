ca7d662ab5982142a96c9e3fb3d9f7876dafea9e
imashnake0 <krpaluru@uwaterloo.ca>
2025-12-15 21:33:01 -0500

    create new settings module

---
+++ settings/build.gradle.kts
@@ -0,0 +1,86 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.detekt)
+}
+
+android {
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    buildFeatures { compose = true }
+
+    namespace = "com.imashnake.animite.settings"
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    compilerOptions.freeCompilerArgs.addAll(
+        "-opt-in=androidx.compose.animation.ExperimentalSharedTransitionApi"
+    )
+}
+
+// TODO: Remove unused modules.
+dependencies {
+    implementation(projects.api.anilist)
+    implementation(projects.api.preferences)
+    implementation(projects.core)
+    implementation(projects.media)
+    implementation(projects.navigation)
+
+    // AndroidX
+    implementation(libs.androidx.activityCompose)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycleRuntimeKtx)
+
+    // Navigation
+    implementation(libs.androidx.navigationCommon)
+
+    // Compose
+    implementation(libs.compose.animation)
+    implementation(libs.compose.foundation)
+    implementation(libs.compose.material)
+    implementation(libs.compose.material.icons.core)
+    implementation(libs.compose.material3)
+    implementation(libs.compose.runtime)
+    implementation(libs.compose.ui)
+    debugImplementation(libs.compose.ui.tooling)
+    implementation(libs.compose.ui.toolingPreview)
+
+    // Compose Markdown
+    implementation(libs.boswelja.composeMarkdown.material3)
+
+    // Cascade Compose
+    implementation(libs.saket.cascade)
+
+    // Coil
+    implementation(libs.bundles.coil)
+
+    // Kotlin
+    implementation(libs.kotlin.coroutines.android)
+    implementation(libs.kotlin.coroutines.core)
+    implementation(libs.kotlinx.serialization.core)
+
+    // Hilt
+    implementation(libs.hilt.android)
+    implementation(libs.hilt.navigationCompose)
+    ksp(libs.hilt.android.compiler)
+
+    testImplementation(libs.test.junit)
+
+    androidTestImplementation(libs.androidx.test.junit)
+    androidTestImplementation(libs.androidx.test.espressoCore)
+    androidTestImplementation(libs.compose.test.ui.testJunit4)
+}
---
+++ settings/src/main/kotlin/com/imashnake/animite/settings/SettingsScreen.kt
@@ -0,0 +1,3 @@
+package com.imashnake.animite.settings
+
+
