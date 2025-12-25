4d0e032dbfe14ae94141a74ef3facb8f568250a2
imashanke0 <krpaluru@uwaterloo.ca>
2025-12-25 04:37:21 -0500

    add theme to prefs

--- api/preferences/src/main/kotlin/com/imashnake/animite/api/preferences/PreferencesRepository.kt
+++ api/preferences/src/main/kotlin/com/imashnake/animite/api/preferences/PreferencesRepository.kt
@@ -2,6 +2,7 @@ package com.imashnake.animite.api.preferences
 
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.core.stringPreferencesKey
 import com.imashnake.animite.api.preferences.ext.getValue
 import com.imashnake.animite.api.preferences.ext.setValue
@@ -10,16 +11,25 @@ import javax.inject.Inject
 class PreferencesRepository @Inject constructor(
     private val dataStore: DataStore<Preferences>
 ) {
+    // region profile
     private val accessTokenKey = stringPreferencesKey("access_token")
-    private val viewerIdKey = stringPreferencesKey("viewer_id")
     val accessToken = dataStore.getValue(accessTokenKey, null)
-    val viewerId = dataStore.getValue(viewerIdKey, null)
-
     suspend fun setAccessToken(accessToken: String?) {
         dataStore.setValue(accessTokenKey, accessToken)
     }
 
+    private val viewerIdKey = stringPreferencesKey("viewer_id")
+    val viewerId = dataStore.getValue(viewerIdKey, null)
     suspend fun setViewerId(viewerId: Int?) {
         dataStore.setValue(viewerIdKey, viewerId.toString())
     }
+    // endregion
+
+    // region settings
+    private val themeKey = intPreferencesKey("theme")
+    val theme = dataStore.getValue(themeKey, null)
+    suspend fun setThemeToken(theme: Int?) {
+        dataStore.setValue(themeKey, theme)
+    }
+    // endregion
 }
