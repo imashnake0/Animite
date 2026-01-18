3f831a0076b739758b8e7d5a146112010a2643cd
imashnake0 <krpaluru@uwaterloo.ca>
2026-01-18 04:10:59 -0500

    day part -> hour in vm

--- settings/src/main/kotlin/com/imashnake/animite/settings/SettingsViewModel.kt
+++ settings/src/main/kotlin/com/imashnake/animite/settings/SettingsViewModel.kt
@@ -3,7 +3,6 @@ package com.imashnake.animite.settings
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.imashnake.animite.api.preferences.PreferencesRepository
-import com.imashnake.animite.core.extensions.DayPart
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -16,7 +15,7 @@ class SettingsViewModel @Inject constructor(
     val theme = preferencesRepository.theme
     val useSystemColorScheme = preferencesRepository.useSystemColorScheme
     val isDevOptionsEnabled = preferencesRepository.isDevOptionsEnabled
-    val dayPart = preferencesRepository.dayPart
+    val dayHour = preferencesRepository.dayHour
 
     fun setTheme(theme: Theme) = viewModelScope.launch(Dispatchers.IO) {
         preferencesRepository.setThemeToken(theme.name)
@@ -29,7 +28,7 @@ class SettingsViewModel @Inject constructor(
         preferencesRepository.setDevOptionsEnabled(enabled)
     }
 
-    fun setDayPart(dayPart: DayPart?) = viewModelScope.launch(Dispatchers.IO) {
-        preferencesRepository.setDayPart(dayPart?.name)
+    fun setDayHour(dayHour: Int?) = viewModelScope.launch(Dispatchers.IO) {
+        preferencesRepository.setDayHour(dayHour)
     }
 }
