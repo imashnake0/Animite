2db0add3dff9dbc152062206ae1059b991ad0cbb
imashnake0 <krpaluru@uwaterloo.ca>
2026-01-18 05:44:35 -0500

    fix

--- core/src/main/kotlin/com/imashnake/animite/core/extensions/InstantExt.kt
+++ core/src/main/kotlin/com/imashnake/animite/core/extensions/InstantExt.kt
@@ -1,10 +1,11 @@
 package com.imashnake.animite.core.extensions
 
 import androidx.compose.ui.util.fastRoundToInt
+import kotlin.math.floor
 import kotlin.time.ExperimentalTime
 
 @OptIn(ExperimentalTime::class)
-fun Float.toDayPart() = when (this.fastRoundToInt()) {
+fun Float.toDayPart() = when (floor(this).fastRoundToInt()) {
     in 6..11 -> DayPart.MORNING
     in 12..17 -> DayPart.AFTERNOON
     in 18..20 -> DayPart.EVENING
