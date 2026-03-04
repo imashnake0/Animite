742b5dbc8176dd44cbe5fbeb6443b42545bf8005
Ian Roberts <Ian.roberts_22@hotmail.co.uk>
2026-03-04 00:27:23 +0000

    Apply suggestions from code review

--- social/build.gradle.kts
+++ social/build.gradle.kts
@@ -11,10 +11,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
 
