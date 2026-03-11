878da0dfb6c2a87ed516abdc6458c894efb33724
imashnake0 <krpaluru@uwaterloo.ca>
2026-03-11 05:13:07 -0400

    cleanup

--- media/src/main/kotlin/com/imashnake/animite/media/ext/Month.kt
+++ media/src/main/kotlin/com/imashnake/animite/media/ext/Month.kt
@@ -4,7 +4,8 @@ import com.imashnake.animite.api.anilist.type.MediaSeason
 import kotlinx.datetime.Month
 
 /**
- * Gets the [Month]'s season.
+ * Gets the [Month]'s season:
+ * [Definition](https://anilist.notion.site/Release-Data-72ae49d8449842868b73520512b5471e).
  */
 val Month.season get() = when(this) {
     Month.JANUARY -> MediaSeason.WINTER
@@ -12,11 +13,11 @@ val Month.season get() = when(this) {
     Month.MARCH -> MediaSeason.WINTER
     Month.APRIL -> MediaSeason.SPRING
     Month.MAY -> MediaSeason.SPRING
-    Month.JUNE -> MediaSeason.SUMMER
+    Month.JUNE -> MediaSeason.SPRING
     Month.JULY -> MediaSeason.SUMMER
     Month.AUGUST -> MediaSeason.SUMMER
-    Month.SEPTEMBER -> MediaSeason.FALL
+    Month.SEPTEMBER -> MediaSeason.SUMMER
     Month.OCTOBER -> MediaSeason.FALL
     Month.NOVEMBER -> MediaSeason.FALL
-    Month.DECEMBER -> MediaSeason.WINTER
+    Month.DECEMBER -> MediaSeason.FALL
 }
