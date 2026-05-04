6e40c24a19daaaa3d47c535cfaf19f7a998ba4d4
imashnake0 <krpaluru@uwaterloo.ca>
2026-05-04 00:26:01 -0400

    more hook up

--- api/anilist/src/main/kotlin/com/imashnake/animite/api/anilist/AnilistMediaRepository.kt
+++ api/anilist/src/main/kotlin/com/imashnake/animite/api/anilist/AnilistMediaRepository.kt
@@ -41,7 +41,8 @@ class AnilistMediaRepository(
         page: Int = 0,
         perPage: Int = 10,
         season: MediaSeason? = null,
-        seasonYear: Int? = null
+        seasonYear: Int? = null,
+        isAdult: Boolean = false
     ): Flow<Result<MediaList>> {
         return apolloClient
             .query(
@@ -51,7 +52,8 @@ class AnilistMediaRepository(
                     perPage = Optional.presentIfNotNull(perPage),
                     sort = Optional.presentIfNotNull(sort),
                     season = Optional.presentIfNotNull(season),
-                    seasonYear = Optional.presentIfNotNull(seasonYear)
+                    seasonYear = Optional.presentIfNotNull(seasonYear),
+                    isAdult = Optional.presentIfNotNull(if (isAdult) null else false)
                 )
             )
             .fetchPolicy(
