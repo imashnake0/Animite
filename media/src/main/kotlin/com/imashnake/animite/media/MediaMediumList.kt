d6168791600d0f78fc1c18a4d7439a065100b446
imashnake0 <krpaluru@uwaterloo.ca>
2026-05-05 01:47:41 -0400

    i

--- media/src/main/kotlin/com/imashnake/animite/media/MediaMediumList.kt
+++ media/src/main/kotlin/com/imashnake/animite/media/MediaMediumList.kt
@@ -41,7 +41,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
