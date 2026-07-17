715a0379dfb49ca3e1e3accfe55ec87218eb5589
imashnake0 <krpaluru@uwaterloo.ca>
2026-07-17 13:46:05 -0400

    cleanup

--- profile/src/main/kotlin/com/imashnake/animite/profile/ui/MediaTrackingList.kt
+++ profile/src/main/kotlin/com/imashnake/animite/profile/ui/MediaTrackingList.kt
@@ -18,11 +18,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.layout.requiredSize
-import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -35,7 +32,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.toShape
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.snapshots.SnapshotStateMap
 import androidx.compose.ui.Alignment
@@ -47,18 +43,13 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.util.fastForEachIndexed
 import com.imashnake.animite.api.anilist.sanitize.media.Media
 import com.imashnake.animite.api.anilist.sanitize.profile.User
 import com.imashnake.animite.core.ui.LocalPaddings
-import com.imashnake.animite.core.ui.component.LoadingMediaSmall
 import com.imashnake.animite.core.ui.component.MediaTrackingCard
-import com.imashnake.animite.core.ui.rememberDefaultPaddings
 import com.imashnake.animite.media.MediaPage
 import com.imashnake.animite.media.ext.res
 import com.imashnake.animite.profile.R
@@ -309,103 +300,7 @@ private fun MediaTrackingItem(
     }
 }
 
-@Composable
-fun LoadingMediaMediumItem(
-    shouldShowRank: Boolean,
-    modifier: Modifier = Modifier,
-) {
-    Box(modifier) {
-        if (shouldShowRank) {
-            Box(
-                Modifier
-                    .background(MaterialTheme.colorScheme.primary.copy(alpha = 0.1f))
-                    .requiredHeight(137.dp)
-                    .width(LocalPaddings.current.large * 3)
-            )
-        }
-
-        Row {
-            if (shouldShowRank)
-                Text(
-                    text = " ",
-                    style = MaterialTheme.typography.bodyMedium,
-                    textAlign = TextAlign.Center,
-                    fontWeight = FontWeight.ExtraBold,
-                    modifier = Modifier
-                        .padding(horizontal = LocalPaddings.current.tiny)
-                        .requiredWidth(30.dp)
-                        .align(Alignment.CenterVertically)
-                )
-
-            LoadingMediaSmall(
-                imageHeight = 137.dp,
-                cardWidth = 96.dp,
-                shouldShowLabel = false
-            )
-        }
-    }
-}
-
-@Composable
-fun LoadingMediaMediumList(
-    count: Int,
-    modifier: Modifier = Modifier,
-    contentPadding: PaddingValues = PaddingValues()
-) {
-    LazyColumn(
-        verticalArrangement = Arrangement.spacedBy(LocalPaddings.current.small),
-        contentPadding = contentPadding,
-        userScrollEnabled = false,
-        modifier = modifier
-    ) {
-        item(key = 0) {}
-        items(count) { index ->
-            LoadingMediaMediumItem(
-                shouldShowRank = true,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .clip(
-                        RoundedCornerShape(
-                            topStart = 18.dp,
-                            bottomStart = 18.dp,
-                            topEnd = if (index == 0) {
-                                18.dp
-                            } else LocalPaddings.current.small,
-                            bottomEnd = if (index == count - 1) {
-                                18.dp
-                            } else LocalPaddings.current.small,
-                        )
-                    )
-                    .background(MaterialTheme.colorScheme.onBackground.copy(alpha = 0.025f))
-            )
-        }
-    }
-}
-
-// TODO: Cleanup
-@Preview
-@Composable
-fun PreviewLoadingMediaMediumList() {
-    CompositionLocalProvider(LocalPaddings provides rememberDefaultPaddings()) {
-        LoadingMediaMediumList(count = 10)
-    }
-}
-
-@Preview
-@Composable
-fun PreviewLoadingMediaMediumItem() {
-    CompositionLocalProvider(LocalPaddings provides rememberDefaultPaddings()) {
-        LoadingMediaMediumItem(
-            shouldShowRank = true,
-            modifier = Modifier
-                .fillMaxWidth()
-                .clip(RoundedCornerShape(LocalPaddings.current.small))
-                .background(MaterialTheme.colorScheme.onBackground.copy(alpha = 0.025f))
-        )
-    }
-}
-
-// TODO: Move this.
+// TODO: Move this and support manga.
 enum class Names {
     COMPLETED,
     PAUSED,
