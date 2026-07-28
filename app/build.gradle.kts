177f8a70bef123788d217fd4f0d2ae8d894206f5
imashnake0 <krpaluru@uwaterloo.ca>
2026-07-27 23:16:51 -0400

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
     defaultConfig {
         applicationId = "com.imashnake.animite"
         versionCode = 50
-        versionName = "0.7.6-alpha02"
+        versionName = "0.7.7-alpha01"
     }
 
     signingConfigs {
