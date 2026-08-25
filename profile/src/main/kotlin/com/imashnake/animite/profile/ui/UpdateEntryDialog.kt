2be55100c40c3c9b6d77b7465d9be2078aab575c
imashnake0 <krpaluru@uwaterloo.ca>
2026-08-25 05:13:57 -0400

    added scroll

--- profile/src/main/kotlin/com/imashnake/animite/profile/ui/UpdateEntryDialog.kt
+++ profile/src/main/kotlin/com/imashnake/animite/profile/ui/UpdateEntryDialog.kt
@@ -21,8 +21,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.Close
@@ -156,10 +158,13 @@ fun UpdateEntryDialog(
 
             Column(
                 verticalArrangement = Arrangement.spacedBy(LocalPaddings.current.large),
-                modifier = Modifier.padding(
-                    horizontal = LocalPaddings.current.large,
-                    vertical = LocalPaddings.current.large
-                )
+                modifier = Modifier
+                    .padding(
+                        horizontal = LocalPaddings.current.large,
+                        vertical = LocalPaddings.current.large
+                    )
+                    // TODO: Make this more graceful.
+                    .verticalScroll(rememberScrollState())
             ) {
                 Section(title = "Status") {
                     StatusDropDown(
